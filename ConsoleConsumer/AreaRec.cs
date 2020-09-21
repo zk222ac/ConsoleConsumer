f036aab3037f206957e286d6d8aeaad9de589d2e
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-09-21 16:51:04 +0200

    Add project files.

---
+++ ConsoleConsumer/AreaRec.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleConsumer
+{
+    public class AreaRec
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        // empty constructor
+        public AreaRec()
+        {
+
+        }
+        // parametric constructor 
+        public AreaRec(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Length);
+            sb.Append(",");
+            sb.Append(Width);
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConsoleConsumer/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Length here ");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert width here ");
+            int width = int.Parse(Console.ReadLine());
+
+            // 2,3
+            AreaRec rect = new AreaRec(length, width);
+            var result = AreaRecAsync(rect).Result;
+
+            Console.WriteLine("Final result is: " + result);
+
+            Console.ReadKey();
+        }
+
+        public static async Task<string> AreaRecAsync(AreaRec rec)
+        {
+            string result = null;
+            using (HttpClient client = new HttpClient())
+            {
+                var jsonString = JsonConvert.SerializeObject(rec);
+
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("https://localhost:44378/Rectangle/AreaRectangle", content);
+
+                if(response.StatusCode != HttpStatusCode.Conflict)
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    result = await response.Content.ReadAsStringAsync();
+
+                }
+                else
+                {
+                    throw new Exception("service is not running");
+                }
+            }
+
+            return result;
+        }
+    }
+}
